5d963e1bbf86f872c56d166825d4bf5537215349
“Santiago <santiagoas77@gmail.com>
2021-04-14 18:17:51 -0500

    Added routing for each area

--- client/src/app/pages/dashboard/dashboard-routing.module.ts
+++ client/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MaestrosComponent } from '../maestros/maestros.component';
+import { BloquesComponent } from 'src/app/components/bloques/bloques.component';
+import { ModulosComponent } from 'src/app/components/modulos/modulos.component';
+import { SalonesComponent } from 'src/app/components/salones/salones.component';
+import { MaestrosComponent } from '../../components/maestros/maestros.component';
+import { MateriasComponent } from '../../components/materias/materias.component';
 
 
 const routes: Routes = [
   {
     path: 'maestros',
     component: MaestrosComponent
+  },
+  {
+    path: 'materias',
+    component: MateriasComponent
+  },
+  {
+    path: 'modulos',
+    component: ModulosComponent
+  },
+  {
+    path: 'bloques',
+    component: BloquesComponent
+  },
+  {
+    path: 'salones',
+    component: SalonesComponent
   },
 ];
 
