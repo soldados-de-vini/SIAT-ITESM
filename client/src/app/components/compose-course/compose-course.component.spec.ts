c1dfca6d524326af37791e7709968edc72460ebc
“Santiago <santiagoas77@gmail.com>
2021-04-26 12:51:33 -0500

    Done with edit, delete and create tests for courses

--- client/src/app/components/compose-course/compose-course.component.spec.ts
+++ client/src/app/components/compose-course/compose-course.component.spec.ts
@@ -1,20 +1,24 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NzMessageService, NzModalRef } from 'ng-zorro-antd';
 import { ApiService } from 'src/app/services/api/api.service';
 import { Overlay } from '@angular/cdk/overlay';
+import { environment } from 'src/environments/environment';
 
 import { ComposeCourseComponent } from './compose-course.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ComposeCourseComponent', () => {
   let component: ComposeCourseComponent;
   let fixture: ComponentFixture<ComposeCourseComponent>;
+  let api: ApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ComposeCourseComponent ],
-      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, BrowserAnimationsModule],
       providers: [
         ApiService,
         NzMessageService,
@@ -39,6 +43,71 @@ describe('ComposeCourseComponent', () => {
     fixture = TestBed.createComponent(ComposeCourseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpTestingController = TestBed.inject(HttpTestingController);
+    api = TestBed.inject(ApiService);
+  });
+
+  it('should create a new course', () => {
+    const testCourse = {
+      key: 'MA100',
+      name: 'Mate 1',
+      capacity: 50,
+      avenue: ['IIC'],
+      typeUF: 'TEC20',
+      semester: '4',
+      initialWeek: 5,
+      weeks: 5
+    };
+
+    component.courseForm.setValue(testCourse);
+    component.addCourse();
+
+    const successResponse = {
+      status: {
+        statusCode: 201,
+        message: 'Created successfully.',
+      }
+    };
+    const request = httpTestingController.expectOne({
+      method: 'POST',
+      url: environment.api_url + '/courses',
+    });
+
+    request.flush(successResponse);
+
+    expect(successResponse.status.statusCode).toBe(201);
+  });
+
+  it('should be able to edit a course', () => {
+    const courseId = 'abcd1234';
+    const testCourse = {
+      key: 'MA100',
+      name: 'Mate 1',
+      capacity: 50,
+      avenue: ['IIC'],
+      typeUF: 'TEC20',
+      semester: '4',
+      initialWeek: 5,
+      weeks: 5
+    };
+    component.isEditing = true;
+    component.courseForm.setValue(testCourse);
+    component.course = testCourse;
+    component.course.id = courseId;
+    component.editCourse();
+
+    const successResponse = {
+      status: {
+        statusCode: 200,
+        message: 'Created successfully.',
+      }
+    };
+    const request = httpTestingController.expectOne({
+      method: 'PUT',
+      url: environment.api_url + `/courses/${courseId}`,
+    });
+    request.flush(successResponse);
+    expect(successResponse.status.statusCode).toBe(200);
   });
 
   it('should create', () => {
--- client/src/app/components/courses/courses.component.spec.ts
+++ client/src/app/components/courses/courses.component.spec.ts
@@ -1,21 +1,28 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NzModalService } from 'ng-zorro-antd';
+import { NzMessageService, NzModalService } from 'ng-zorro-antd';
 import { Overlay } from '@angular/cdk/overlay';
+import { ApiService} from 'src/app/services/api/api.service';
+import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CoursesComponent } from './courses.component';
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
   let fixture: ComponentFixture<CoursesComponent>;
+  let api: ApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CoursesComponent ],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, HttpClientTestingModule, BrowserAnimationsModule],
       providers: [
         NzModalService,
         Overlay,
+        ApiService,
+        NzMessageService
       ]
     })
     .compileComponents();
@@ -25,6 +32,40 @@ describe('CoursesComponent', () => {
     fixture = TestBed.createComponent(CoursesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    api = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be able to delete a course', () => {
+    const courseId = 'abcd1234';
+    const deleteCourse = (id) => {
+      api.delete(`/courses/${id}`).subscribe(
+        (response) => {
+          if (response.status?.statusCode === 200){
+            console.log('Materia borrado con éxito');
+          } else {
+            console.log('Ha ocurrido un erorr');
+          }
+        }
+      );
+    };
+    deleteCourse(courseId);
+
+    const request = httpTestingController.expectOne({
+      method: 'DELETE',
+      url: environment.api_url + `/courses/${courseId}`,
+    });
+
+    const successResponse = {
+      status: {
+        statusCode: 200,
+        message: 'Deleted successfully.',
+      }
+    };
+
+    request.flush(successResponse);
+
+    expect(successResponse.status.statusCode).toBe(200);
   });
 
   it('should create', () => {
