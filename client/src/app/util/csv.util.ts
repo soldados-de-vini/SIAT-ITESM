ba8493478e6a471772cc6990576a46c773de5ebe
Diego <diegoar7@live.com>
2021-04-28 20:48:52 -0500

    csv utils and csv uploader component

---
+++ client/src/app/util/csv.util.ts
@@ -0,0 +1,23 @@
+/**
+ * Function to convert a csv string to and object
+ *
+ * @param csv csv as string
+ */
+export function convertCsvToObject(csv: string, areArray: Array<number>) {
+  const lines = csv.split('\n');
+  const result = [];
+  const headers = lines[0].split(',');
+  for (let i = 1; i < lines.length - 1; i++) {
+    const obj = {};
+    const currentline = lines[i].trim().split(',');
+    for (let j = 0; j < headers.length; j++) {
+      if (areArray.includes(j)){
+        obj[headers[j].trim()] = currentline[j].split('|');
+      } else {
+        obj[headers[j].trim()] = currentline[j];
+      }
+    }
+    result.push(obj);
+  }
+  return result;
+}
