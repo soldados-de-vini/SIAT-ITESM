5a525861ff9993105844ede48b88bc920e0c5cf1
Hivini <vinicioquinterobuss@gmail.com>
2021-05-19 23:12:03 +0000

    Added Helmet and CORS configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,31 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
+const WHITELIST = ['http://localhost/'];
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(helmet())
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: function (origin, callback) {
+      let corsOptions;
+      if (WHITELIST.indexOf(origin) !== -1) {
+        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+      } else {
+        corsOptions = { origin: false }; // disable CORS for this request
+      }
+      callback(null, corsOptions);
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('SIAT API')
     .setDescription('The API of the SIAT Tec system.')
@@ -23,9 +44,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
