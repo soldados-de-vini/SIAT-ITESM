0a6de1443d654deb8d0931fcab7ef63335e10f6a
“Santiago <santiagoas77@gmail.com>
2021-05-24 13:56:55 -0500

    Added select for blocks

--- client/src/app/components/compose-block/compose-block.component.ts
+++ client/src/app/components/compose-block/compose-block.component.ts
@@ -18,16 +18,19 @@ export class ComposeBlockComponent implements OnInit {
   public modules: Array<Module>;
   public loading: boolean;
   public blockForm: FormGroup;
+  avenues: any;
 
   constructor(
     private apiService: ApiService,
     private formBuilder: FormBuilder,
     private nzModalRef: NzModalRef,
-    private nzMessageService: NzMessageService
+    private nzMessageService: NzMessageService,
+    private api: ApiService
   ) { }
 
   ngOnInit(): void {
     this.getModules();
+    this.getAvenues();
     if (this.block) {
       this.createFormWithBlock();
     } else {
@@ -142,5 +145,11 @@ export class ComposeBlockComponent implements OnInit {
 
   public isArray(object: any): boolean{
     return Array.isArray(object);
+  }
+
+  public getAvenues(){
+    this.api.get('/avenues').subscribe((res) => {
+      this.avenues = res.result;
+    });
   }
 }
