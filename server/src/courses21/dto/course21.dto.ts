47867ba628a4e544a132c14282943ab5939863e8
Vinicio Quintero <vinicioquinterobuss@gmail.com>
2021-06-09 08:10:22 -0600

    Solved an issue on the DTO request

--- server/src/courses21/dto/course21.dto.ts
+++ server/src/courses21/dto/course21.dto.ts
@@ -1,7 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Course20Dto } from '../../courses20/dto/course20.dto';
 
-export class Course21Dto extends Course20Dto {
+export class Course21Dto {
+  @ApiProperty()
+  @IsNotEmpty()
+  key: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  capacity: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  initialWeek: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  weeks: number;
+
+  @ApiProperty()
+  semester: string;
+
+  @ApiProperty()
+  avenue: string[];
+
+  @ApiProperty()
+  typeUF: string;
+
   @ApiProperty()
   modules: string[];
 }
